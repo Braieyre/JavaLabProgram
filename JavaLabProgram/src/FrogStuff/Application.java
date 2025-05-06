15063776bb5c8c6e97a0cb0578ad9274e7bce7ac
admin <admin@DESKTOP-7SP5I4O>
2025-05-06 21:17:22 +0800

    With frog game~

---
+++ JavaLabProgram/src/FrogStuff/Application.java
@@ -0,0 +1,31 @@
+package FrogStuff;
+
+import java.util.Scanner;
+
+public class Application
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("欢迎来到青蛙跳小游戏");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("请输入要挑战的局数，便于为您计算胜率");
+		int times = sc.nextInt();
+		System.out.println("请分别输入青蛙距离目标的距离和最大的跳跃次数");
+		int goal = sc.nextInt();
+		int max = sc.nextInt();
+		while (goal <= 0 || max <= 0) 
+		{
+			System.out.println("请输入正整数数值");
+			goal = sc.nextInt();
+			max = sc.nextInt();
+		}
+
+		FrogSimulation sim = new FrogSimulation(goal, max);
+		double rate = sim.runSimulations(times);
+		System.out.println("胜率为：" + rate + "!");
+		sc.close();
+		
+	}
+}
--- JavaLabProgram/src/FrogStuff/FrogSimulation.java
+++ JavaLabProgram/src/FrogStuff/FrogSimulation.java
@@ -1,12 +1,13 @@
 package FrogStuff;
 
+import java.util.Scanner;
+
 public class FrogSimulation
 {
 											/** 从起始位置到目标的距离，以英寸为单位。*/
 		private int goalDistance;		/** 允许达到目标的最大跳数。 */
 		private int maxHops;
-		int hopDis
-		int hopTimes = 0;
+		private int hopDis;
 												/*** FrogSimulation的构造方法，其中 dist 是从开始到目标的距离，
 												 ***numHops 是允许到达目标的最大跳数.
 												 *** dist > 0; numHops > 0  */
@@ -15,25 +16,62 @@ public class FrogSimulation
 				goalDistance = dist;
 				maxHops = numHops;
 		}
-		public void getDistance(int hopDis)
-		{
-			this.hopDis = hopDis;
-		}
 					/***返回一个整数，表示青蛙跳跃时要移动的距离.*/
 		private int hopDistance()
 		{
+			System.out.println("青蛙跳了" + hopDis + "英寸~");
+			System.out.println();
 			return hopDis;
-		}
+		}//despite hopDis worked, I will still use hopDistance(), to make sure OOP
 						/***模拟一只青蛙试图达到（1）部分所述的目标.
 						 			*如果青蛙在模拟过程中成功到达或通过目标，则返回 true，否则返回false; */
 		public boolean simulate()
 		{
-			
+			int rest = goalDistance;		//傀儡量，不伤害原值
+			Scanner sc = new Scanner(System.in);
+			int count = 0;		//记录消极地带最后一次挣扎
+			for(int i = 1; i <= maxHops; i++)
+			{
+				System.out.println("请输入第"+ i +"次青蛙跳的距离~");
+				hopDis = sc.nextInt();
+				rest -= hopDistance();
+				if(rest <= 0 ) 
+				{
+					return true;
+				}
+				if(rest > goalDistance) 
+				{
+					if(count == 1)
+					{
+						return false;
+					}
+					count++;
+					System.out.println("青蛙已经进入消极地带，Last chance~");
+				}
+			}
+			return false;
 		}
 					/***运行 num 模拟并返回青蛙在其中的模拟比例成功达到或通过目标.
 					 	*** num > 0 */
 		public double runSimulations(int num)
 		{
-					/* 将在(2)部分实现 */
+			int win = 0;
+			for (int i = 1; i <= num; i++) 
+			{
+				System.out.println("——————————————————————————");
+				System.out.println("第" + i + "局~");
+				boolean result = simulate();
+				if (result)
+				{
+					System.out.println(result + ", You win!!!");
+					System.out.println();
+					win++;
+				} else 
+				{
+					System.out.println(result + ", You lose!!!");
+					System.out.println();
+				}
+			}
+			return (double)win/num;
 		}
 }
